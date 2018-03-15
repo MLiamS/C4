d24d592ec061a0b5e9a5d149bf67ee3710d0dc1a
Liam Stabeno <LiamStabeno@gmail.com>
2018-03-15 11:44:45 -0700

    add the cell value when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ function Board(ctx, width, height) {
   this.ctx           = ctx;
   this.width         = width;
   this.height        = height;
-  this.cellwidth     = this.width/7;
-  this.cellheight    = this.height/6;
+  this.cellWidth     = this.width/7;
+  this.cellHeight    = this.height/6;
   this.grid = [];
     for (var x = 0; x < 7; x ++) {
       this.grid[x] = [];
@@ -19,14 +19,14 @@ function Board(ctx, width, height) {
 Board.prototype.draw = function() {
   this.ctx.lineWidth = 3;
 
-  for (var x         = this.cellwidth; x < this.width; x = x+this.cellwidth) {
+  for (var x         = this.cellWidth; x < this.width; x = x+this.cellWidth) {
     this.ctx.beginPath();
     this.ctx.moveTo(x,0);
     this.ctx.lineTo(x, this.height);
     this.ctx.stroke();
   };
 
-  for (var y         = this.cellheight; y < this.height; y = y+this.cellheight) {
+  for (var y         = this.cellHeight; y < this.height; y = y+this.cellHeight) {
     this.ctx.beginPath();
     this.ctx.moveTo(0,y);
     this.ctx.lineTo(this.width, y);
@@ -43,15 +43,15 @@ $(document).ready(function() {
   var canvas         = document.getElementById('canvas');
   var ctx            = canvas.getContext('2d');
   canvas.addEventListener("mousedown", getPos);
-  var gameboard        = new Board(ctx, canvas.width, canvas.height);
 
-  gameboard.draw();
+  var gameBoard        = new Board(ctx, canvas.width, canvas.height);
 
-  function getPos(event)
-
-  {
-    console.log("works" + event)
-
-};
+  function getPos(event) {
+    var rect = canvas.getBoundingClientRect();
+    var x = Math.floor((event.clientX - rect.left)/gameBoard.cellWidth);
+    var y = Math.floor((event.clientY - rect.top)/gameBoard.cellHeight);
+    console.log("cell x,y..." + "   " + x + " " + y);
+  };
+    gameBoard.draw();
 
 });
