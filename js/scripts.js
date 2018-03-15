a8a6b340c1b466f9e2949ea83998a8d3f8eb6ce3
Liam Stabeno <LiamStabeno@gmail.com>
2018-03-15 10:41:52 -0700

    draw the board split into cells

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,44 @@
 //Back-End
 
-function Board(ctx, width, height)  {
-  this.ctx = ctx;
-  this.width = width;
-  this.height = height;
+function Board(ctx, width, height) {
+  this.ctx           = ctx;
+  this.width         = width;
+  this.height        = height;
+  this.cellwidth     = this.width/7;
+  this.cellheight    = this.height/6;
+
+  
 };
 
 Board.prototype.draw = function() {
+  this.ctx.lineWidth = 3;
 
-this.ctx.moveTo(0,0);
-this.ctx.lineTo(45,600);
-this.ctx.stroke();
+  for (var x         = this.cellwidth; x < this.width; x = x+this.cellwidth) {
+    this.ctx.beginPath();
+    this.ctx.moveTo(x,0);
+    this.ctx.lineTo(x, this.height);
+    this.ctx.stroke();
+  };
+
+  for (var y         = this.cellheight; y < this.height; y = y+this.cellheight) {
+    this.ctx.beginPath();
+    this.ctx.moveTo(0,y);
+    this.ctx.lineTo(this.width, y);
+    this.ctx.stroke();
+  };
 
 
-
-}
+};
 
 
 
 //Front-End
 $(document).ready(function() {
-  // console.log('Ready');
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
+  var canvas         = document.getElementById('canvas');
+  var ctx            = canvas.getContext('2d');
 
-var gameboard = new Board(ctx, canvas.width, canvas.height);
+  var gameboard        = new Board(ctx, canvas.width, canvas.height);
 
-gameboard.draw();
+  gameboard.draw();
 
 });
