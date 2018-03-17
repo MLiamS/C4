caeb143bfdd11c2cbc6e096a244f60db091c6ffe
Liam <Liam@Liams-MacBook.local>
2018-03-17 15:28:09 -0700

    add arrays to keep track of cols number of checkers

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-//Back-End
+//Back-
+A = [];
+B = [];
+C = [];
+D = [];
+E = [];
+F = [];
+G = [];
 
 function Board(ctx, width, height) {
   this.ctx           = ctx;
@@ -19,39 +26,57 @@ function Board(ctx, width, height) {
 Board.prototype.draw = function() {
   this.ctx.lineWidth = 3;
 
-  for (var x         = this.cellWidth; x < this.width; x = x+this.cellWidth) {
+  for (var x = this.cellWidth; x < this.width; x = x+this.cellWidth) {
     this.ctx.beginPath();
     this.ctx.moveTo(x,0);
     this.ctx.lineTo(x, this.height);
     this.ctx.stroke();
   };
 
-  for (var y         = this.cellHeight; y < this.height; y = y+this.cellHeight) {
+  for (var y = this.cellHeight; y < this.height; y = y+this.cellHeight) {
     this.ctx.beginPath();
     this.ctx.moveTo(0,y);
     this.ctx.lineTo(this.width, y);
     this.ctx.stroke();
   };
+};
 
+function dropChecker(x,y) {
+  if (x == 0) { A.push(true) };
+  if (x == 1) { B.push(true) };
+  if (x == 2) { C.push(true) };
+  if (x == 3) { D.push(true) };
+  if (x == 4) { E.push(true) };
+  if (x == 5) { F.push(true) };
+  if (x == 6) { G.push(true) };
 
 };
 
 
 
+
+
 //Front-End
 $(document).ready(function() {
-  var canvas         = document.getElementById('canvas');
-  var ctx            = canvas.getContext('2d');
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
   canvas.addEventListener("mousedown", getPos);
 
-  var gameBoard        = new Board(ctx, canvas.width, canvas.height);
+  var gameBoard = new Board(ctx, canvas.width, canvas.height);
+
+
+  // checkBoard()
 
   function getPos(event) {
     var rect = canvas.getBoundingClientRect();
     var x = Math.floor((event.clientX - rect.left)/gameBoard.cellWidth);
     var y = Math.floor((event.clientY - rect.top)/gameBoard.cellHeight);
     console.log("cell x,y..." + "   " + x + " " + y);
+
+    dropChecker(x,y);
+
   };
     gameBoard.draw();
+
 
 });
