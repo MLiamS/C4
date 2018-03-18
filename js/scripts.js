a92c98e0a102a4c570d476cf395d6391d05084eb
Liam <Liam@Liams-MacBook.local>
2018-03-18 15:49:25 -0700

    add ceilings to cols so users can only play 6 checkers per col

--- js/scripts.js
+++ js/scripts.js
@@ -43,25 +43,29 @@ Board.prototype.draw = function() {
 };
 
 function dropCheckerRed(x) {
-  if (x == 0) { A.push("R") };
-  if (x == 1) { B.push("R") };
-  if (x == 2) { C.push("R") };
-  if (x == 3) { D.push("R") };
-  if (x == 4) { E.push("R") };
-  if (x == 5) { F.push("R") };
-  if (x == 6) { G.push("R") };
+  if (x == 0 && A.length < 6) { A.push("R") };
+  if (x == 1 && B.length < 6) { B.push("R") };
+  if (x == 2 && C.length < 6) { C.push("R") };
+  if (x == 3 && D.length < 6) { D.push("R") };
+  if (x == 4 && E.length < 6) { E.push("R") };
+  if (x == 5 && F.length < 6) { F.push("R") };
+  if (x == 6 && G.length < 6) { G.push("R") };
   Turn ++;
+
+  console.log("A-[" + A  + "] B-[" + B  + "] " + "C-[" + C +"] " + "D-[" + D + "] " + "E-[" + E  + "] " + "F-[" + F + "]  G-[" + G + "]");
 };
 
 function dropCheckerYel(x) {
-  if (x == 0) { A.push("Y") };
-  if (x == 1) { B.push("Y") };
-  if (x == 2) { C.push("Y") };
-  if (x == 3) { D.push("Y") };
-  if (x == 4) { E.push("Y") };
-  if (x == 5) { F.push("Y") };
-  if (x == 6) { G.push("Y") };
+  if (x == 0 && A.length < 6) { A.push("Y") };
+  if (x == 1 && B.length < 6) { B.push("Y") };
+  if (x == 2 && C.length < 6) { C.push("Y") };
+  if (x == 3 && D.length < 6) { D.push("Y") };
+  if (x == 4 && E.length < 6) { E.push("Y") };
+  if (x == 5 && F.length < 6) { F.push("Y") };
+  if (x == 6 && G.length < 6) { G.push("Y") };
   Turn ++;
+
+  console.log("A-[" + A  + "] B-[" + B  + "] " + "C-[" + C +"] " + "D-[" + D + "] " + "E-[" + E  + "] " + "F-[" + F + "]  G-[" + G + "]");
 };
 
 
