3ac0443835c07898da71de5dac9711e9ebdaa3e9
Liam Stabeno <LiamStabeno@gmail.com>
2018-03-19 16:39:13 -0700

    refactor broken state

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 //Back-
-A = [];
-B = [];
-C = [];
-D = [];
-E = [];
-F = [];
-G = [];
-var Turn = 0;
-
 function Board(ctx, width, height) {
   this.ctx           = ctx;
   this.width         = width;
   this.height        = height;
   this.cellWidth     = this.width/7;
   this.cellHeight    = this.height/6;
+  this.A = [];
+  this.B = [];
+  this.C = [];
+  this.D = [];
+  this.E = [];
+  this.F = [];
+  this.G = [];
+  this.Turn = 0;
+
   this.grid = [];
     for (var x = 0; x < 7; x ++) {
       this.grid[x] = [];
@@ -21,11 +21,46 @@ function Board(ctx, width, height) {
         this.grid[x][y] = "";
       };
     };
-
 };
+
+Board.prototype.dropChecker = function (x,y)  {
+
+}
+
+Board.prototype.animateChecker = function(x, y) {
+  var amount = 0;
+  var c = this.ctx;
+  var centerX = x*this.cellWidth + this.cellWidth/2;
+  var centerY = y*this.cellHeight + this.cellHeight/2;
+  var radius = (this.cellWidth * (1.5/5));
+  var circleInt = setInterval(function() {
+    amount += 0.05;
+    if (amount > 1) {
+      amount = 1;
+      clearInterval(circleInt);
+    };
+    c.clearRect(0, 0, this.width, this.height);
+    c.beginPath();
+    c.arc(centerX, centerY, radius, 0, (2*Math.PI)*amount);
+    c.stroke();
+  }, 20);
+};
+
+// if (gameBoard.Turn%2 == 0) {
+//   gamneboard.dropCheckerRed(x);
+// }
+//   else {
+//     dropCheckerYel(x)
+// };
+
 
 Board.prototype.draw = function() {
   this.ctx.lineWidth = 3;
+
+  if (this.A.length > 0) {
+          this.animateChecker(x, y);
+        }
+
 
   for (var x = this.cellWidth; x < this.width; x = x+this.cellWidth) {
     this.ctx.beginPath();
@@ -42,7 +77,7 @@ Board.prototype.draw = function() {
   };
 };
 
-function dropCheckerRed(x) {
+Board.prototype.dropCheckerRed = function(x, y) {
   if (x == 0 && A.length < 6) { A.push("R") };
   if (x == 1 && B.length < 6) { B.push("R") };
   if (x == 2 && C.length < 6) { C.push("R") };
@@ -52,10 +87,11 @@ function dropCheckerRed(x) {
   if (x == 6 && G.length < 6) { G.push("R") };
   Turn ++;
 
-  console.log("A-[" + A  + "] B-[" + B  + "] " + "C-[" + C +"] " + "D-[" + D + "] " + "E-[" + E  + "] " + "F-[" + F + "]  G-[" + G + "]");
+
+  console.log("A-[" + this.A  + "] B-[" + B  + "] " + "C-[" + C +"] " + "D-[" + D + "] " + "E-[" + E  + "] " + "F-[" + F + "]  G-[" + G + "]");
 };
 
-function dropCheckerYel(x) {
+Board.prototype.dropCheckerYel = function(x, y) {
   if (x == 0 && A.length < 6) { A.push("Y") };
   if (x == 1 && B.length < 6) { B.push("Y") };
   if (x == 2 && C.length < 6) { C.push("Y") };
@@ -68,8 +104,14 @@ function dropCheckerYel(x) {
   console.log("A-[" + A  + "] B-[" + B  + "] " + "C-[" + C +"] " + "D-[" + D + "] " + "E-[" + E  + "] " + "F-[" + F + "]  G-[" + G + "]");
 };
 
-
-
+// function updateBoard() {
+//   if (A.length > 0) {
+//
+//
+//   }
+// }
+//
+// };
 
 
 //Front-End
@@ -86,16 +128,9 @@ $(document).ready(function() {
   function getPos(event) {
     var rect = canvas.getBoundingClientRect();
     var x = Math.floor((event.clientX - rect.left)/gameBoard.cellWidth);
-    // var y = Math.floor((event.clientY - rect.top)/gameBoard.cellHeight);
+    var y = Math.floor((event.clientY - rect.top)/gameBoard.cellHeight);
+    gameBoard.dropChecker(x,y);
     console.log("column "  + x);
-
-    if (Turn%2 == 0) {
-      dropCheckerRed(x);
-    }
-      else {
-        dropCheckerYel(x)
-    };
-
   };
     gameBoard.draw();
 
